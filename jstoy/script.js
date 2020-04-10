9f6e0559eac3ed8c28db3d7f34f71b56d2ee1660
Paul <paul@saturn.home>
2020-04-10 11:37:01 +0300

    jstoy script demo

---
+++ jstoy/script.js
@@ -0,0 +1,52 @@
+'use strict';
+void function ($form, $out) {
+    if(typeof localStorage === 'object') {
+        $form.elements['code'].value = localStorage.getItem('code');
+        $form.elements['save'].disabled = true;
+    } else {
+        $out.innerHTML = 'warning: unable to use local storage';
+    }
+    $form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var print = function () {
+            var $line = document.createElement('div');
+            $line.className = 'printline';
+            $line.innerHTML = Array.prototype.slice.call(arguments).join(' ');
+            document.getElementById('print').appendChild($line);
+        };
+        var load = function(fname, callback) {
+            var $script = document.createElement('script');
+            $script.addEventListener('load', function () {
+                this.parentNode.removeChild(this);
+                try {
+                    $out.innerHTML = callback();
+                } catch(exception) {
+                    print(fname + ': ', exception);
+                    typeof(exception.stack) === 'string' && print(exception.stack);
+                }
+            });
+            $script.addEventListener('error', function () {
+                this.parentNode.removeChild(this);
+                print(fname, ': failed to load');
+            });
+            $script.src = fname;
+            document.querySelector('head').appendChild($script);
+        };
+        document.getElementById('print').innerHTML = '';
+        try {
+            $out.innerHTML = eval($form.elements['code'].value);
+        } catch(exception) {
+            $out.innerHTML = exception;
+            typeof(exception.stack) === 'string' && print(exception.stack);
+        }
+    }.bind(undefined));
+    $form.elements['save'].addEventListener('click', function (event) {
+        if(typeof localStorage === 'object') {
+            localStorage.setItem('code', $form.elements['code'].value);
+            $form.elements['save'].disabled = true;
+        }
+    });
+    $form.elements['code'].addEventListener('change', function (event) {
+        $form.elements['save'].disabled = false;
+    });
+}(document.getElementById('input'), document.getElementById('result'));
