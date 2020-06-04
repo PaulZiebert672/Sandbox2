5f33dbf4d1b3494471dd009962276ac79e8ef7da
Paul <paul@saturn.home>
2020-06-04 08:49:45 +0300

    jsdoc added

--- samples/promise/callback.js
+++ samples/promise/callback.js
@@ -1,25 +1,49 @@
-var list = [
-  { name: 'Object 1'},
-  { name: 'Object 2'},
-];
-var date0 = Date.now();
-var printTime = (mark) => print(mark , 'elapsed time:', Date.now() - date0, 'ms.');
+void function() {
+  /**
+   * list of sample objects
+   * @type {Object}[]
+   */
+  var list = [
+    { name: 'Object 1'},
+    { name: 'Object 2'},
+  ];
 
-var viewList = function () {
-  setTimeout(function () {
-    printTime('~~>');
-    list.forEach(item => print(item.name));
-  }, 500);
-};
+  /**
+   * initial time moment
+   * @type {Number}
+   */
+  var date0 = Date.now();
 
-var addItem = function (item, callback) {
-  setTimeout(function() {
-    printTime('::>');
-    list.push(item);
-    callback && callback();
-  }, 1000);
-};
+  /**
+   * prints elapsed time marked by the given string
+   * @param {String} mark 
+   */
+  var printTime = (mark) => print(mark , 'elapsed time:', Date.now() - date0, 'ms.');
 
-//addItem({ name: 'Object A' });
-//viewList();
-addItem({ name: 'Object A' }, viewList);
+  /**
+   * view contents of the list
+   */
+  var viewList = function () {
+    setTimeout(function () {
+      printTime('~~>');
+      list.forEach(item => print(item.name));
+    }, 500);
+  };
+
+  /**
+   * add item to the list
+   * @param {Object} item 
+   * @param {Function} callback 
+   */
+  var addItem = function (item, callback) {
+    setTimeout(function() {
+      printTime('::>');
+      list.push(item);
+      callback && callback();
+    }, 1000);
+  };
+
+  //addItem({ name: 'Object A' });
+  //viewList();
+  addItem({ name: 'Object A' }, viewList);
+}();
--- samples/promise/promise.js
+++ samples/promise/promise.js
@@ -1,28 +1,52 @@
-var list = [
-  { name: 'Object 1'},
-  { name: 'Object 2'},
-];
-var date0 = Date.now();
-var printTime = (mark) => print(mark , 'elapsed time:', Date.now() - date0, 'ms.');
+void function () {
+  /**
+   * list of sample objects
+   * @type {Object}[]
+   */
+  var list = [
+    { name: 'Object 1'},
+    { name: 'Object 2'},
+  ];
 
-var viewList = function () {
-  setTimeout(function () {
-    printTime('~~>');
-    list.forEach(item => print(item.name));
-  }, 500);
-};
+  /**
+   * initial time moment
+   * @type {Number}
+   */
+  var date0 = Date.now();
 
-var addItem = function (item) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function() {
-      printTime('::>');
-      list.push(item);
-      var err = false;
-      (!err)? resolve(): reject('Error occured');
-    }, 1000);
-  });
-};
+  /**
+   * prints elapsed time marked by the given string
+   * @param {String} mark 
+   */
+  var printTime = (mark) => print(mark , 'elapsed time:', Date.now() - date0, 'ms.');
 
-addItem({ name: 'Object A' })
-  .then(viewList)
-  .catch(err => print(err));
+  /**
+   * view contents of the list
+   */
+  var viewList = function () {
+    setTimeout(function () {
+      printTime('~~>');
+      list.forEach(item => print(item.name));
+    }, 500);
+  };
+
+  /**
+   * add item to the list
+   * @param {Object} item
+   * @returns {Promise} 
+   */
+  var addItem = function (item) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function() {
+        printTime('::>');
+        list.push(item);
+        var err = false;
+        (!err)? resolve(): reject('Error occured');
+      }, 1000);
+    });
+  };
+
+  addItem({ name: 'Object A' })
+    .then(viewList)
+    .catch(err => print(err));
+}();
