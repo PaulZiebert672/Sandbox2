270910f480bd6a0d5c13440d23265cb40b0062e1
paul.ziebert <paul.ziebert@mail.ru>
2020-07-22 15:17:45 +0300

    remove initial style

--- samples/var/test-loadCss.js
+++ samples/var/test-loadCss.js
@@ -9,6 +9,14 @@ if(!document.head.querySelector('#id12')) {
 
 //document.head.removeChild(document.head.querySelector('#id12'));
 
+//document.head.removeChild(document.head.querySelector('#id12'));
+var $initLink = [].slice.call(document.head.querySelectorAll('link'))
+  .filter(function (el) {
+    var tgt = "style.css";
+    return el.href.slice(-(tgt.length)) == tgt;
+  })[0];
+//$initLink && document.head.removeChild($initLink);
+
 load([
   'https://code.jquery.com/jquery-3.5.1.js',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.js'
