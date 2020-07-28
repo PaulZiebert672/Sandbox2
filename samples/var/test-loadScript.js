99675ea2cdf5c7084db5f97497b25e490261be5a
PaulZiebert672 <paul.ziebert@mail.ru>
2020-07-28 08:50:03 +0300

    sample script

--- samples/var/test-loadScript.js
+++ samples/var/test-loadScript.js
@@ -1,14 +1,23 @@
-print(`
-<style>.strange {font-size: 18pt; font-family: Helvetica; color: blue; padding-left: 12pt;}</style>
-<div class="big strange">111</div>
-<div class="small strange">222</div>
-`);
+var cdnJquery = 'https://code.jquery.com/jquery-3.5.1.js';
+var cdnBootstrap = {
+  css: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css',
+  js: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.js'
+};
 
-load([
-  'https://zeptojs.com/zepto.js',
-  'https://underscorejs.org/underscore.js'
-], function () {
-  _.each($('.strange'), function (el) {
-    print($(el).attr('class'));
+link(cdnBootstrap.css);
+print(heredoc(function () { /*
+<style>.productInStockList {font-size: 1.25em; color: red;}</style>
+<div class="row"><div class="col-md-1 offset-md-1 productInStockList">113</div></div>
+<div class="row"><div class="col-md-1 offset-md-1 productInStockList">204</div></div>
+<div class="row"><label class="col-md-1">Amount:</label><div class="col-md-1 productInStockList">317</div></div>
+<div class="row"><button class="col-md-1 offset-md-2 btn btn-outline-secondary">Go!</button></div>
+*/ }));
+
+load([cdnJquery, cdnBootstrap.js], function () {
+  if(!$('body > div.container-fluid').length) {
+    $('body').wrapInner('<div class="container-fluid"></div>');
+  }
+  $('.productInStockList').each((idx, el) => {
+    print($(el).text(), $(el).attr('class'));
   });
 });
