a6393b53c8b24f1ae9f029ff8625e0c9a6474a86
paul.ziebert <paul.ziebert@mail.ru>
2021-11-02 16:31:27 +0300

    proxy for post requests

---
+++ samples/node.sample-02/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan');
+const axios = require('axios');
+const http = require('http');
+const app = express();
+
+const port = process.env.APP_PORT || 6781;
+
+// Set middleware
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('combined'));
+
+app.post("*", async (req, res) => {
+    console.log('headers',  req.headers);
+    console.log('body:', req.body);
+    try {
+        var upshot = await axios.post('http://localhost:6780', req.body, {
+            headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json'
+            }
+        });
+        console.log(upshot.status);
+        if(upshot.status != 200) {
+            throw new Error(`status = ${upshot.status}`);
+        }
+        return res.json(upshot.data);
+    } catch(err) {
+        console.error('connection error:' + err.message);
+        return res.status(502).json({ msg: err.message });
+    }
+});
+
+http.createServer(app).listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+});
