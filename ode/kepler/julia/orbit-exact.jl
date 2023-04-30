af71367161a17a42bc3ec08b38e6397074f881f5
PaulZiebert672 <paul.ziebert@mail.ru>
2023-04-30 16:36:05 +0300

    corrections in julia script for kerpler orbit

--- ode/kepler/julia/orbit-exact.jl
+++ ode/kepler/julia/orbit-exact.jl
@@ -1,6 +1,7 @@
 using Plots;
+using LaTeXStrings;
 
-eps = 0.28
+eps = 0.6
 N = 120
 
 function keplerPsi(phi, eps)
@@ -21,18 +22,25 @@ for i in 1:N
     a[i, 2] = sqrt(1 - eps^2)*sin(psi)
 end
 
+gr(size = (300, 300))
+Plots.scalefontsizes(0.45)
 p = plot(
     a[:, 1],
     a[:, 2],
     seriestype=:scatter,
     aspect_ratio = 1,
-    xlabel = "x",
-    ylabel = "y",
+    xlabel = L"x",
+    ylabel = L"y",
     plot_title = "Kepler problem",
-    markersize = 2,
+    markersize = 1.5,
     legend = false,
     framestyle = :origin,
     dpi = 300
 )
-annotate!(-1.5, 0.85, "eps = $(eps)")
+x = eps # one-symbol var required
+annotate!(
+    xlims(p)[1] + 0.07*(xlims(p)[2] - xlims(p)[1]),
+    ylims(p)[2] - 0.03*(ylims(p)[2] - ylims(p)[1]),
+    L"\epsilon = %$x"
+)
 savefig(p, "kepler-orbit.png")
