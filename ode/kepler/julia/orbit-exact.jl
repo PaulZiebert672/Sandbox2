f4b12c434a92fab5fa23be0b025171bbab498248
PaulZiebert672 <paul.ziebert@mail.ru>
2023-05-02 09:32:37 +0300

    kepler problem. exact solutions

--- ode/kepler/julia/orbit-exact.jl
+++ ode/kepler/julia/orbit-exact.jl
@@ -15,16 +15,19 @@ function keplerPsi(phi, eps)
     theta1
 end
 
-a = Array{Float64}(undef, N, 2)
+a = Array{Float64}(undef, N, 4)
 for i in 1:N
     psi = keplerPsi((i/N)*2*pi, eps)
     a[i, 1] = cos(psi) - eps
     a[i, 2] = sqrt(1 - eps^2)*sin(psi)
+    a[i, 3] = -sin(psi)/(1 - eps*cos(psi))
+    a[i, 4] = sqrt(1 - eps^2)*cos(psi)/(1 - eps*cos(psi))
 end
 
 gr(size = (300, 300))
+Plots.scalefontsizes()
 Plots.scalefontsizes(0.45)
-p = plot(
+plotOrbit = plot(
     a[:, 1],
     a[:, 2],
     seriestype=:scatter,
@@ -37,10 +40,39 @@ p = plot(
     framestyle = :origin,
     dpi = 300
 )
-x = eps # one-symbol var required
 annotate!(
-    xlims(p)[1] + 0.07*(xlims(p)[2] - xlims(p)[1]),
-    ylims(p)[2] - 0.03*(ylims(p)[2] - ylims(p)[1]),
-    L"\epsilon = %$x"
+    xlims(plotOrbit)[1] + 0.07*(xlims(plotOrbit)[2] - xlims(plotOrbit)[1]),
+    ylims(plotOrbit)[2] - 0.03*(ylims(plotOrbit)[2] - ylims(plotOrbit)[1]),
+    L"\epsilon = %$eps%$"
 )
-savefig(p, "kepler-orbit.png")
+savefig(plotOrbit, "kepler-orbit.png")
+
+function rho(x, y)
+    sqrt(x^2 + y^2)
+end
+
+gr(size = (300, 300))
+Plots.scalefontsizes()
+Plots.scalefontsizes(0.45)
+plotRadial = plot(
+    map(eachrow(a)) do s 
+        rho(s[1], s[2])
+    end,
+    map(eachrow(a)) do s 
+        (s[3]*s[1] + s[2]*s[4])/rho(s[1], s[2])
+    end,
+    seriestype=:scatter,
+    aspect_ratio = 1,
+    xlabel = L"r",
+    ylabel = L"p_r",
+    plot_title = "Kepler problem",
+    markersize = 1.5,
+    legend = false,
+    dpi = 300
+)
+annotate!(
+    xlims(plotRadial)[2] - 0.12*(xlims(plotRadial)[2] - xlims(plotRadial)[1]),
+    ylims(plotRadial)[2] - 0.03*(ylims(plotRadial)[2] - ylims(plotRadial)[1]),
+    L"\epsilon = %$eps%$"
+)
+savefig(plotRadial, "kepler-radial.png")
