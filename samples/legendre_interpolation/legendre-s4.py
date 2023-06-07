41d877e3711fd30362defb94eda0a7f7923303a1
PaulZiebert672 <paul.ziebert@mail.ru>
2023-06-07 22:13:13 +0300

    some corrections

--- samples/legendre_interpolation/legendre-s4.py
+++ samples/legendre_interpolation/legendre-s4.py
@@ -2,18 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from numpy.polynomial import Polynomial
+from typing import Tuple
 
-def legendre_poly(k):
+def legendre_poly(k:int) -> Tuple[Polynomial, Polynomial]:
     coeff = np.array([[-1/16, 1/24, 1/4, -1/6], [9/16, -9/8, -1/4, 1/2], [9/16, 9/8, -1/4, -1/2], [-1/16, -1/24, 1/4, 1/6]])
+    res_prime = [coeff[k][1], 2*coeff[k][2], 3*coeff[k][3]]
     res = coeff[k]
-    return Polynomial(res)
+    return (Polynomial(res), Polynomial(res_prime))
 
+# Create interpolation polynomials of 3-rd order
 x = np.linspace(-2, 3, 201)
-p_m32 = legendre_poly(0)
-p_m12 = legendre_poly(1)
-p_p12 = legendre_poly(2)
-p_p32 = legendre_poly(3)
+p_m32, p_prime_m32 = legendre_poly(0)
+p_m12, p_prime_m12 = legendre_poly(1)
+p_p12, p_prime_p12 = legendre_poly(2)
+p_p32, p_prime_p32 = legendre_poly(3)
 
+# Plot interpolation polynomials
 fig = plt.figure(figsize=(8, 8))
 plt.title("Legendre interpolation polynomials")
 #plt.axis("equal")
@@ -23,10 +27,14 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
 
-plt.plot(x, p_m32(x))
-plt.plot(x, p_m12(x))
-#plt.plot(x, p_p12(x))
-#plt.plot(x, p_p32(x))
-plt.legend(['L_{-3/2}', 'L_{-1/2}', 'L_{1/2}', 'L_{3/2}'])
+plt.plot(x, p_m32(x), label="L_{-3/2}")
+plt.plot(x, p_m12(x), label="L_{-1/2}")
+#plt.plot(x, p_p12(x), label="L_{1/2}")
+#plt.plot(x, p_p32(x), label="L_{3/2}")
+plt.plot(x, p_prime_m32(x), linestyle='dotted', label="L'_{-3/2}")
+plt.plot(x, p_prime_m12(x), linestyle='dotted', label="L'_{-1/2}")
+#plt.plot(x, p_prime_p12(x), linestyle='dotted', label="L'_{1/2}")
+#plt.plot(x, p_prime_p32(x), linestyle='dotted', label="L'_{3/2}")
+plt.legend()
 
 fig.savefig('sample-04.png', dpi=150)
