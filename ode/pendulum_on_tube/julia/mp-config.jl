835837126548094844b4b44f700d91bc5c7e62b3
PaulZiebert672 <paul.ziebert@mail.ru>
2024-09-28 19:33:17 +0300

    constant of motion

--- ode/pendulum_on_tube/julia/mp-config.jl
+++ ode/pendulum_on_tube/julia/mp-config.jl
@@ -1,2 +1,3 @@
-N = 120
+N = 120     # points per orbital period
+K = 6       # total number of cycles
 u0 = [-acos(1 - 0.5), 0.0]
--- ode/pendulum_on_tube/julia/mp-hamilton.jl
+++ ode/pendulum_on_tube/julia/mp-hamilton.jl
@@ -1,37 +1,42 @@
-using DifferentialEquations
+using OrdinaryDiffEq
+using LinearAlgebra
+using ForwardDiff
 using Elliptic
 using Plots
 using Statistics
 
 include("mp-config.jl")
 
-function pendulum(du, u, p, t)
-    du[1] = u[2]
-    du[2] = -sin(u[1])
-end
+# Hamiltonian
+H(p, q) = 0.5*(p[1])^2 + 1 - cos(q[1])
 
-energy(u) = 0.5*u[2]^2 + 1 - cos(u[1])
+# Hamilton's canonical equations
+pdot(dp, p, q, params, t) = ForwardDiff.gradient!(dp, q -> -H(p, q), q)
+qdot(dq, p, q, params, t) = ForwardDiff.gradient!(dq, p -> H(p, q), p)
 
-e0 = energy(u0)
+q0 = [u0[1]]
+p0 = [u0[2]]
+
+e0 = H(p0, q0)
 t_period = 4*Elliptic.K(0.5*e0)
 println("energy = ", e0)
 println("period = ", t_period)
-t_step = t_period/N 
-t_max = K*t_period
+t_step = t_period/N
+t_max = t_period*K
 
-prob = ODEProblem(pendulum, u0, (0.0, t_max))
-sol = solve(prob, Vern7(), adaptive=false, dt=t_step)
-#display(sol)
+prob = DynamicalODEProblem(pdot, qdot, p0, q0, (0.0, t_max))
+sol = solve(prob, KahanLi6(), dt = t_step);
+data = reduce(hcat, sol.u)'
 
 gr(size = (300, 300))
 Plots.scalefontsizes()
 Plots.scalefontsizes(0.4)
 plotOrbit = scatter(
-    Tuple.(sol.u),
+    data[:, 2], data[:, 1],
     aspect_ratio = 1,
-    xlabel = "\\theta",
-    ylabel = "\\omega",
-    plot_title = "Mathematical pendulum - Lagrangian dynamics",
+    xlabel = "\$\\theta\$",
+    ylabel = "p",
+    plot_title = "Mathematical pendulum - Hamiltonian dynamics",
     markersize = 1.0,
     alpha = 0.25,
     legend = false,
@@ -42,24 +47,26 @@ annotate!(
     ylims(plotOrbit)[2] - 0.06*(ylims(plotOrbit)[2] - ylims(plotOrbit)[1]),
     text("\$\\mathcal{E}_0 = $(round(e0, digits=4))\$\n\$T = $(round(t_period, digits=6))\$", :crimson, :right, 4)
 )
-#gui(), readline()
-savefig(plotOrbit, "mp-lagrange-orbit.png")
+# gui(), readline()
+savefig(plotOrbit, "mp-hamilton-orbit.png")
 
-constant_of_motion = map(energy, sol.u)
+constant_of_motion = map(H, data[:, 2], data[:, 1])
 # display(constant_of_motion)
 println("mean energy = ", mean(constant_of_motion))
 println("std energy = ", std(constant_of_motion))
 
+Plots.scalefontsizes()
+Plots.scalefontsizes(0.4)
 plotError = scatter(
-    sol.t, (constant_of_motion .- energy(u0))./energy(u0),
+    sol.t, (constant_of_motion .- e0)./e0,
     xlabel = "Time",
     ylabel = "Relative energy error",
-    plot_title = "Mathematical pendulum - Lagrangian dynamics",
-    markersize = 0.8,
-    alpha = 0.15,
+    plot_title = "Mathematical pendulum - Hamiltonian dynamics",
+    markersize = 0.5,
+    alpha = 0.25,
     legend = false,
     dpi = 300,
     annotation = (t_max - t_period, 0.0, text("\$\\mathcal{E}_0 = $(round(e0, digits=4))\$\n\$T = $(round(t_period, digits=6))\$", :darkslateblue, :bottom, 4))
 )
 # gui(), readline()
-savefig(plotError, "mp-lagrange-error.png")
+savefig(plotError, "mp-hamilton-error.png")
