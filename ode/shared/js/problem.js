502268c5f09bb3a246adc547117d6f0ebccb4c3b
PaulZiebert672 <paul.ziebert@mail.ru>
2024-10-23 21:17:46 +0300

    van der Pohl oscillator. params in config

--- ode/shared/js/problem.js
+++ ode/shared/js/problem.js
@@ -7,6 +7,7 @@ if(typeof require === 'function') {
     VoidCode.Problem.LotkaVolterra = require('./problem-lv.js');
     VoidCode.Problem.KeplerProblem = require('./problem-kepler.js');
     VoidCode.Problem.DoublePendulum = require('./problem-dpm.js');
+    VoidCode.Problem.VanDerPohlOscillator = require('./problem-vdpo.js');
 }
 
 /* short aliases */
@@ -14,6 +15,7 @@ VoidCode.Problem.mp = VoidCode.Problem.MathematicalPendulum;
 VoidCode.Problem.lv = VoidCode.Problem.LotkaVolterra;
 VoidCode.Problem.kepler = VoidCode.Problem.KeplerProblem;
 VoidCode.Problem.dpm = VoidCode.Problem.DoublePendulum;
+VoidCode.Problem.vdpo = VoidCode.Problem.VanDerPohlOscillator;
 
 if(typeof module === 'object') {
     module.exports = VoidCode.Problem;
