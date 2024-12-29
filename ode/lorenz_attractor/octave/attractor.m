2b987f55194ef981f613bd718ac813baa9349759
PaulZiebert672 <paul.ziebert@mail.ru>
2024-12-29 19:45:53 +0300

    lorenz attactor xz plane

--- ode/lorenz_attractor/octave/attractor.m
+++ ode/lorenz_attractor/octave/attractor.m
@@ -1,8 +1,8 @@
 % Lorenz attractor
 
+rho = 28;
 sigma = 10;
 beta = 8/3;
-rho = 28;
 
 f = @(t, u) [
   -sigma*u(1) + sigma*u(2);
@@ -10,13 +10,18 @@ f = @(t, u) [
   -beta*u(3) + u(1)*u(2)
 ];
 
-[t, u] = ode45(f, [0 100], [1 0 0]);     % Runge-Kutta 4th/5th order ODE solver
+% Dormand-Prince adaptive ODE solver
+opt = odeset("MaxStep", 0.002);
+[t, u] = ode45(f, [0 24], [1 1 10], opt);
 
-scatter3(u(:, 1), u(:, 2), u(:, 3), 1.2, 'r') % marker size and color
-view (30, 30)      % azimuth and elevation
-xlabel "x"
-ylabel "y"
-zlabel "z"
-title("Lorenz attractor")
-print("orbit.jpg", "-S960,720");
+% Plot orbit
+function hf = mk_orbit_plot(x, y)
+  plot(x, y)
+  xlabel "x"
+  ylabel "z"
+  title("Lorenz attractor")
+  print("orbit.jpg", "-S960,720");
+endfunction
+
+mk_orbit_plot(u(:, 1), u(:, 3));
 
